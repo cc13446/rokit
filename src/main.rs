02501c4a09d136be570cd791ba8944ce753f3ea8
cc13446 <1344613920@qq.com>
2022-03-23 16:47:35 +0800

    实验iced

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{executor, Application, Command, Element, Settings, Text, Clipboard};
+
+pub fn main() -> iced::Result {
+    Rokit::run(Settings::default())
+}
+
+struct Rokit;
+
+impl Application for Rokit {
+    type Executor = executor::Default;
+    type Message = ();
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Rokit, Command<Self::Message>) {
+        (Rokit, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Rokit")
+    }
+
+    fn update(&mut self, _message: Self::Message, _clipboard: &mut Clipboard) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Text::new("Hello, world!").into()
+    }
 }
